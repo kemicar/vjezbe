347e1deb2648f7372af780a6f161e9a4ab91d7e7
LeonaVerasto <165012069+LeonaVerasto@users.noreply.github.com>
2024-03-26 16:29:46 +0100

    Add files via upload

---
+++ vjezba_1_LeonaVerasto.R
@@ -0,0 +1,52 @@
+x<-2
+y<-1
+x+y
+z<-y+x
+z
+print(z)
+X='R'
+Y<-'test'
+paste(X,Y)
+x
+class(x)
+z<-0.2
+class(z)
+logical <-TRUE
+class(logical)
+class(x)
+class(X)
+x
+x=as.character(x)
+vec1 <-c(1,2,3,4,5,6,7,8,9)
+vec2 <-c(T,F,FALSE,TRUE)
+
+vec3 <-c('severe', 'mild','control')
+vec4<-as.factor(c('severe', 'mild','control'))
+lenght(vec2)
+lenght(vec2)
+length(vec2)
+vec2
+vec2(2)
+vec2[1]
+vec1[2:4]
+vec1[c(F,T,F,F,F,F,F,F)]
+vec3
+vec3[-2]
+vec3
+vec<-vec3[-2]
+vec3<-vec3[-2]
+rm(vec1)
+vec1
+vec6<-c(1,2,3,4,-1,-2,0.1,-0,1)
+abs(vec6)
+max(vec6)
+?sqrt
+vec7<-c(1.237,2.944,3,4,5.5222)
+ceiling(vec7)
+round(vec7,2)
+cos(vec7)
+print('Hello world')
+
+
+
+
