f532cebcd2fb85c96c69ce5d6494434dda08eb8b
Martinasevo <165011640+Martinasevo@users.noreply.github.com>
2024-03-26 17:13:10 +0100

    Add files via upload

---
+++ VJEZBA 1.-Ševo.R
@@ -0,0 +1,39 @@
+2+2
+
+
+x <- 2
+y <- 1
+x+y
+
+z <- y+x
+print(z)
+
+
+x <- "R"
+y <- "test"
+paste(x,y)
+print(paste(x,y)) 
+
+
+class(x)
+vec1 <- c(1,2,3,4,5,6,7,8,9)
+length(vec1)
+vec1[2]
+vec1[c(1,4)]
+vec1[2:4]
+vec1[-2]
+
+
+vec2 <- c(1,2,3,-1,-2,0.1,-0.1)
+abs(vec2)
+max(vec2)
+
+
+
+vec3 <- c(1,237,2.944,3,4,5.55222)
+ceiling(vec3)
+round(vec3,2)
+cos(vec3)
+
+
+print("Hello WORLD")
