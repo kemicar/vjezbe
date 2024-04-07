68f1a05648ea3ac68ec755a1c35a57dfb0d14349
Martinasevo <165011640+Martinasevo@users.noreply.github.com>
2024-04-07 20:47:44 +0200

    Zadaća2-Ševo.R

--- Zadaća2-Ševo.R
+++ Zadaća2-Ševo.R
@@ -9,9 +9,9 @@ rezultati_statisticki <- aov(weight ~ feed, data = chickwts)
 summary(rezultati_statisticki)
 
 
-p_vrijednost <- summary(rezultati_statisticki)$`Pr(>F)`[1]
+p_vrijednost <- summary(rezultati_statisticki)[[1]]$`Pr(>F)`[1]
 p_vrijednost
 
 #korišten je ANOVA test jer smo imali više numeričkih grupa
-#testom je pokazano da je p-vrijednost nula što test čini značajnim
+#testom je pokazano da je p-vrijednost manja od 0.05 što test čini značajnim
 #što potvrđuje hipotezu da težina pileta ovisi o vrsti hrane
